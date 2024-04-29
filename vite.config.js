146a8465793a07e22e9f98e1a585012df8ed01ed
dhanjay arya <dhanjayarya20@gmail.com>
2024-04-30 00:31:58 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const isProduction = process.env.NODE_ENV === 'production';
-
+/
 export default defineConfig({
   server: {
     proxy: isProduction ? {
       '/api': 'https://backendproject-sd57.onrender.com'
     } : {
-      '/api': 'https://backendproject-sd57.onrender.com'
+      '/api': 'http://localhost:3000'
     }
+  
   },
+
   plugins: [react()],
 });
